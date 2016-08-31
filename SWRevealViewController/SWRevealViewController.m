b95065fa41d9f89b9d64c36b7dbb7b95872aa0b5
Peter Malmgren <pmalmgren@safaribooksonline.com>
2016-08-31 12:00:00 -0400

    updated things to ios8

--- SWRevealViewController/SWRevealViewController.m
+++ SWRevealViewController/SWRevealViewController.m
@@ -468,6 +468,10 @@ static CGFloat scaledValue( CGFloat v1, CGFloat min2, CGFloat max2, CGFloat min1
     return _view.bounds;
 }
 
+- (CGAffineTransform)targetTransform {
+    return self.targetTransform;
+}
+
 @end
 
 
